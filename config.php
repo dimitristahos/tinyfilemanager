735d222ed813eed8d211f3ef965d412977282438
Dimitris Tahos <dimitristahos@gmail.com>
2024-12-29 21:34:19 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,9 @@
 <?php
 // User is authenticated, continue with file manager configuration
 $use_auth = false; // Since we've already handled auth above
+
+// Root path for file manager
+$root_path = $_SERVER['DOCUMENT_ROOT'] . 'data';
+
+// Root url for links in file manager
+$root_url = 'http://popx6.gr';
